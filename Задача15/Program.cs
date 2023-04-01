c9c13498d7b32bd1e454e5367ab25a04bbe49349
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-04-01 16:10:30 +0300

    Решил задачу 15

--- Задача15/Program.cs
+++ Задача15/Program.cs
@@ -4,5 +4,40 @@
 7 -> да
 1 -> нет
 */
-Console.WriteLine("Введите число от 1 до 7");
-Console.ReadLine();
+Console.WriteLine("Введите число от 1 до 7 и проверь выходной или нет");
+int number = int.Parse(Console.ReadLine());
+string a = "Будни";
+string b = "Выходной";
+
+if (number == 1)
+{
+    Console.Write(a);
+}
+if (number == 2)
+{
+    Console.Write(a);
+}
+if (number == 3)
+{
+    Console.Write(a);
+}
+if (number == 4)
+{
+    Console.Write(a);
+}
+if (number == 5)
+{
+    Console.Write(a);
+}
+if (number == 6)
+{
+    Console.Write(b);
+}
+if (number == 7)
+{
+    Console.Write(b);
+}
+if (number < 1 | number > 7)
+{
+    Console.Write("Такого дня не существует");
+}
