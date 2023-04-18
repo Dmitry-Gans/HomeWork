b2e48f5b4e994ea97e74f9c41c595d69b2c4d879
Dmitry <dmitry.guslov-gans@yandex.ru>
2023-04-18 22:53:10 +0300

    Решил 25 задачу

---
+++ задача25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Factorial(a,b));
+
+int Factorial(int a, int b)
+{// Вводим временную переменную
+    int tmp = a;
+    for (int i = 1; i < b; i++)
+    {//Возводим А в степень при помощи временной перменной
+        a = a * tmp ;
+    }
+    return a;
+}
